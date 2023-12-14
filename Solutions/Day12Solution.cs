6dfb412a2b57df5f5ec989fd2d0d0e796e08f90b
Gessio Mori Neto <gessiomorin@gmail.com>
2023-12-14 07:50:56 -0300

    Day12PartB

--- Solutions/Day12Solution.cs
+++ Solutions/Day12Solution.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using System.Collections.Concurrent;
 
 namespace Main.Solutions
 {
     internal class Day12Solution : ISolution
     {
+        ConcurrentDictionary<string, long> cache = [];
         public string RunPartA(string[] inputData)
         {
             int total = 0;
@@ -30,28 +31,64 @@ namespace Main.Solutions
 
         public string RunPartB(string[] inputData)
         {
-            int total = 0;
+            long total = 0;
 
             Parallel.ForEach(inputData, line =>
             {
-                (string sequence, int[] numbers) = ProcessLineAndRepeat(line);
+                (string sequence, int[] springs) = ProcessLineAndRepeat(line);
 
-                List<string> listOfSequences = [];
+                long matches = CountMatches(sequence, springs);
 
-                List<string> listOfInvalids = [];
-
-                CreateListOfPossibleSequences(sequence, listOfSequences);
-
-                foreach (string testSequence in listOfSequences)
-                {
-                    if (IsSequenceValidWithMemo(testSequence, numbers, listOfInvalids))
-                    {
-                        Interlocked.Increment(ref total);
-                    }
-                }
+                Interlocked.Add(ref total, matches);
             });
 
             return total.ToString();
+        }
+
+        private long CountMatches(string pattern, int[] springs)
+        {
+            if (pattern == "")
+            {
+                return springs.Length == 0 ? 1 : 0;
+            }
+
+            if (springs.Length == 0)
+            {
+                return pattern.Contains('#') ? 0 : 1;
+            }
+
+            string key = $"{pattern}_{string.Join(",", springs)}";
+
+            if (cache.TryGetValue(key, out long cVal))
+            {
+                return cVal;
+            }
+
+            long result = 0;
+
+            if (pattern[0] == '.' || pattern[0] == '?')
+            {
+                result += CountMatches(pattern[1..], springs);
+            }
+
+            if (pattern[0] == '#' || pattern[0] == '?')
+            {
+                if (springs[0] <= pattern.Length && !pattern[..springs[0]].Contains('.') &&
+                    (springs[0] == pattern.Length || pattern[springs[0]] != '#'))
+                {
+                    int[] newSprings = new int[springs.Length - 1];
+                    Array.Copy(springs, 1, newSprings, 0, newSprings.Length);
+
+                    string newPattern = springs[0] + 1 > pattern.Length
+                        ? ""
+                        : pattern[(springs[0] + 1)..];
+
+                    result += CountMatches(newPattern, newSprings);
+                }
+            }
+
+            cache.TryAdd(key, result);
+            return result;
         }
 
         public static (string, int[]) ProcessLine(string line)
@@ -64,11 +101,9 @@ namespace Main.Solutions
 
         public static (string, int[]) ProcessLineAndRepeat(string line)
         {
-            string sequence = line.Split(' ')[0];
+            string sequence = string.Join("?", Enumerable.Repeat(line.Split(' ')[0], 5));
             int[] numbers = line.Split(' ')[1].Split(',').Select(int.Parse).ToArray();
 
-
-            sequence = string.Join("?", Enumerable.Repeat(sequence, 5));
             int[] repeatedNumbers = Enumerable.Repeat(numbers, 5).SelectMany(x => x).ToArray();
 
             return (sequence, repeatedNumbers);
@@ -128,55 +163,6 @@ namespace Main.Solutions
                 return true;
             }
 
-            return false;
-        }
-
-        public static bool IsSequenceValidWithMemo(string sequence, int[] springs, List<string> listOfInvalids)
-        {
-            int count = 0;
-
-            int currentGroup = 0;
-
-            bool isCounting = false;
-
-            string currentPart = "";
-
-            foreach (char c in sequence + '.')
-            {
-                currentPart += c;
-
-                if (listOfInvalids.Contains(currentPart))
-                {
-                    return false;
-                }
-
-                if (c == '#')
-                {
-                    isCounting = true;
-                    count++;
-                }
-                else if (c == '.' && isCounting)
-                {
-                    isCounting = false;
-                    if (currentGroup < springs.Length && count == springs[currentGroup])
-                    {
-                        currentGroup++;
-                        count = 0;
-                    }
-                    else
-                    {
-                        listOfInvalids.Add(currentPart);
-                        return false;
-                    }
-                }
-            }
-
-            if (currentGroup == springs.Length)
-            {
-                return true;
-            }
-
-            listOfInvalids.Add(currentPart);
             return false;
         }
     }

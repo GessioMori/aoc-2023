0a0a898746b0bbfd959b340eaabf97518182d671
Gessio Mori Neto <gessiomorin@gmail.com>
2023-12-15 22:25:01 -0300

    Day15PartB

--- Solutions/Day15Solution.cs
+++ Solutions/Day15Solution.cs
@@ -1,4 +1,6 @@
 ﻿using Main.Tools;
+using System.Reflection.Emit;
+using System.Text.RegularExpressions;
 
 namespace Main.Solutions
 {
@@ -12,14 +14,7 @@ namespace Main.Solutions
 
             foreach (string word in listOfWords)
             {
-                int currentValue = 0;
-
-                foreach (char ch in word)
-                {
-                    currentValue = ((currentValue + (int)ch) * 17) % 256;
-                }
-
-                total += currentValue;
+                total += RunHashAlgorithm(word);
             }
 
             return total.ToString();
@@ -27,12 +22,78 @@ namespace Main.Solutions
 
         public string RunPartB(string[] inputData)
         {
-            throw new NotImplementedException();
+            Dictionary<int, List<Lens>> boxes = Enumerable.Range(0, 256).ToDictionary(k => k, _ => new List<Lens>());
+
+            foreach (string word in GetListOfWords(inputData[0]))
+            {
+                Match match = Regex.Match(word, @"([a-zA-Z]+)([=-])(\d*)");
+
+                if (match.Success)
+                {
+                    string label = match.Groups[1].Value;
+                    int boxNumber = RunHashAlgorithm(label);
+                    int index = boxes[boxNumber].FindIndex(l => l.Label == label);
+
+                    if (match.Groups[2].Value == "-")
+                    {
+                        if (index != -1)
+                        {
+                            boxes[boxNumber].RemoveAt(index);
+                        }
+                    }
+                    else
+                    {
+                        int focalLength = int.Parse(match.Groups[3].Value);
+                        if (index == -1)
+                        {
+                            boxes[boxNumber].Add(new Lens()
+                            {
+                                FocalLength = focalLength,
+                                Label = label
+                            });
+                        }
+                        else
+                        {
+                            boxes[boxNumber][index].FocalLength = focalLength;
+                        }
+                    }
+                }
+            }
+
+            long total = 0L;
+
+            foreach (var box in boxes)
+            {
+                for (int i = 0; i < box.Value.Count; i++)
+                {
+                    total += (box.Key + 1) * (i + 1) * box.Value[i].FocalLength;
+                }
+            }
+
+            return total.ToString();
         }
 
         public static List<string> GetListOfWords(string inputLine)
         {
             return inputLine.Split(',').ToList();
         }
+
+        public static int RunHashAlgorithm(string word)
+        {
+            int currentValue = 0;
+
+            foreach (char ch in word)
+            {
+                currentValue = ((currentValue + ch) * 17) % 256;
+            }
+
+            return currentValue;
+        }
+    }
+
+    internal class Lens
+    {
+        public required string Label;
+        public required int FocalLength;
     }
 }
